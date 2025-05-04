73b481ad6aa5803afd4229564e91f98536de726b
[PC] Angus Goucher <gus.goucher@gmail.com>
2025-05-04 17:22:33 -0400

    added icon and fixed some misc

--- app.py
+++ app.py
@@ -1,40 +1,15 @@
 from flask import Flask, render_template, g
 import sqlite3
 
-DEVELOPMENT_ENV = True
+DEVELOPMENT_ENV = False
 DB_PATH = "songs.db"
 
 app = Flask(__name__)
-
-app_data = {
-    "name": "Peter's Starter Template for a Flask Web App",
-    "description": "A basic Flask app using bootstrap for layout",
-    "author": "Peter Simeth",
-    "html_title": "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords": "flask, webapp, template, basic",
-}
-
 
 @app.route("/")
 def index():
     songs = query_db("SELECT * FROM songs")
     return render_template("index.html", app_data=app_data, songs=songs)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", app_data=app_data)
-
-
-@app.route("/service")
-def service():
-    return render_template("service.html", app_data=app_data)
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", app_data=app_data)
 
 def init_db():
     with app.app_context():
@@ -63,4 +38,4 @@ def close_connection(exception):
 
 
 if __name__ == "__main__":
-    app.run(debug=DEVELOPMENT_ENV)
+    app.run()
